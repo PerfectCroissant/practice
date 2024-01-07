e488d74bd19e7d064a21343c47555dfc6ee3dc03
PerfectCroissant <kat.ste2016@yandex.ru>
2024-01-07 23:02:54 +0300

    #5

--- project.py
+++ project.py
@@ -1,2 +1,6 @@
 def FindingMaxOfList(spisok):
     return max(spisok)
+
+cnt = int(input("Введите количество элементов массива: "))
+spisok = input("Введите элементы массива: ").split()[:cnt]
+print("Максимум: " + FindingMaxOfList(spisok))
